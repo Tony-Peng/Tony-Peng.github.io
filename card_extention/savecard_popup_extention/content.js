9210c18e71868ed6977e6c38f712ead9ad6d000a
Ruinan <nancywng@sina.com>
2019-09-23 13:52:04 -0400

    extension store function

--- card_extention/savecard_popup_extention/content.js
+++ card_extention/savecard_popup_extention/content.js
@@ -1,37 +1,25 @@
 
-chrome.runtime.sendMessage({todo: "connectFirebase"}, function (response) {
-    console.log("nancy: ", response);
+window.addEventListener('load', function () {
+    //request name property: todo, value: showpageaction
+    chrome.runtime.sendMessage({todo: "showPageAction"});
+    chrome.runtime.sendMessage({todo: "connectFirebase"});
+
     chrome.storage.local.get(['id'], function(result) {
       console.log('Value currently is ' + result.id);
     });
-})
-
-//request name property: todo, value: showpageaction
-chrome.runtime.sendMessage({todo: "showPageAction"});
-
-window.addEventListener('load', function () {
-    var id = localStorage.getItem("id");
-    var cookie = document.cookie;
-    // console.log(cookie);
-    console.log(cookie.split("=")[1]);
-
-    console.log("id: ",id);
 
     if (document.getElementById("save-card-for-future-use-0")) {
         var ghost_trails_div = document.createElement("div"); 
         ghost_trails_div.id = "ghost_trails_div";
         ghost_trails_div.setAttribute("style","background-color:#ffcccc;height:300px;margin-top:40px");
         document.getElementsByClassName("save-card-for-future-use")[0].appendChild(ghost_trails_div);
-        console.log("will show ghost trails next"); 
         showGhostTrails();
     }
 
     //fake submit
     var validation_div = document.getElementsByClassName("chatHelp")[0];
     var submit_wrapper_div = document.getElementsByClassName("submit-button-wrapper")[0];
-    console.log("wrapper: ", submit_wrapper_div);
     var submit_button = submit_wrapper_div.getElementsByClassName("button")[0];
-    console.log("submit button: ", submit_button);
 
     submit_button.addEventListener('click', function(e) {
         e.preventDefault();
@@ -42,8 +30,6 @@ window.addEventListener('load', function () {
 
 
 function showGhostTrails() {
-
-    console.log("show ghost trails");
 
     var checkbox = document.getElementById("save-card-for-future-use-0");
     var ghost_trails_div = document.getElementById("ghost_trails_div");
@@ -58,8 +44,6 @@ function showGhostTrails() {
                 "<ul'><li style='display:list-item;font-size:16px;color:black'><u>Most users</u> regretted their decision to save their credit card information </li>" +
                 "<li style='display:list-item;font-size:16px;color:black'><u>Most users</u> reported that their credit card data was compromised</li></ul>" +
             "</div>";
-
-            console.log("checked");
     }
 
     checkbox.addEventListener('change', function() {
@@ -73,7 +57,6 @@ function showGhostTrails() {
                 "<ul'><li style='display:list-item;font-size:16px;color:black'><u>Most users</u> regretted their decision to save their credit card information </li>" +
                 "<li style='display:list-item;font-size:16px;color:black'><u>Most users</u> reported that their credit card data was compromised</li></ul>" +
             "</div>";
-            console.log("checked");
 
         } else {
             // Checkbox is not checked..
@@ -85,7 +68,6 @@ function showGhostTrails() {
                 "<ul'><li style='display:list-item;font-size:16px;color:black'><u>Most users</u> regretted their decision to save their credit card information </li>" +
                 "<li style='display:list-item;font-size:16px;color:black'><u>Most users</u> reported that their credit card data was compromised</li></ul>" +
             "</div>";
-            console.log("unchecked");
         }
     });
 }
